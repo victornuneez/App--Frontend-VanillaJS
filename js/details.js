286d850f3fc4a7759bc1454f9312ac504b442d38
Victor Nunez <tu_correo@gmail.com>
2026-05-08 19:26:54 -0300

    feat: agregar boton de volver a pagina principal

--- js/details.js
+++ js/details.js
@@ -1,6 +1,11 @@
+import { initSearch } from './search-results.js'
+
+// Funcion que genera la estructura base del componente de detalles
 const detailsComponent = () => {
     return `
     <div id="information-link">
+        <button id="btn-back" class="btn-secondary">Volver</button>
+
         <div id="details-container">
             <!--Aca se inyectan las info detalladas dinamicamente-->        
         </div>
@@ -9,7 +14,7 @@ const detailsComponent = () => {
 };
 
 
-
+// Funcion auxiliar que crea y devuelve la lista de comentarios que tiene cada enlace.
 const displayComments = (comments) => {
     if(comments.length === 0) {
         return [];
@@ -22,6 +27,7 @@ const displayComments = (comments) => {
     return result;
 }
 
+// Funcion que renderiza la vista de detalles del enlace seleccionado
 const renderDetails = (linkDetails) => {
     const container = document.getElementById('details-container');
 
@@ -54,6 +60,7 @@ const renderDetails = (linkDetails) => {
     container.innerHTML = detailHTML;
 }
 
+// Funcion que obtiene todos los detalles del enlace indicado por el usuario
 const getLink = async (linkID) => {
     try {
         if(!linkID) {
@@ -72,33 +79,26 @@ const getLink = async (linkID) => {
         console.error('Hubo un error: ', error.message);
     }
 
-} 
+}; 
 
-// 
-const initDetail = async (linkID) => {
-    try {
-        const app = document.getElementById('app'); // obtenemos el contenedor donde insertaremos nuestros componentes
-        app.innerHTML = detailsComponent();  // Inyectamos nuestro componente base
+
+// Funcion que captura los clicks en boton de volver y renderiza de nuevo la vista de busqueda por filtros.
+const captureClicksBack = async () => {
+    const container = document.getElementById('information-link');
     
-        // funcion que debe obtener toda la informacion de un link por medio de su ID.
-        const links = await getLink(linkID);
+    container.addEventListener('click', (event) => {
 
-        if (!links) {
-            throw new Error('Error al obtener los detalles');
+        if(event.target.classList.contains('btn-secondary')) {
+            initSearch();
         }
-        
-        renderDetails(links); // renderizar la informacion detallada del item seleccionado
-        captureClicksDetails();
-    } catch (error) {
-        console.error("Hubo un problema: ", error.message);
-    }
+    })
 };
 
-export { initDetail };
 
+// Funcion que captura los clicks para votar un enlace y agregar comentarios.
 const captureClicksDetails = () => {
     const container = document.getElementById('details-container');
-
+    
     container.addEventListener('click', async (event) => {
         if (event.target.classList.contains('btn-vote')) {
             const id = event.target.dataset.id
@@ -121,23 +121,26 @@ const captureClicksDetails = () => {
     });
 };
 
+
+// Funcion auxiliar para votar un enlace, usando la API
 const vote = async (id) => {
     try {
         if(!id) {
             throw new Error('Hubo un problema con el id para votar');
         }
-
+        
         const response = await fetch(`http://localhost:3000/api/vote/${id}`, { method:'PATCH' });
         if(!response.ok) throw new Error('Hubo un problema al votar');
 
         const updatedVote = await response.json();
         return updatedVote;
-    
+        
     }catch (error) {
         console.error("Error al votar: ", error.message);
     }
 };
 
+// Funcion auxiliar para comentar los enlaces usando la API.
 const addComment = async (id, text) => {
     try {
         if(!id || !text) {
@@ -151,13 +154,36 @@ const addComment = async (id, text) => {
             },
             body : JSON.stringify({text: text}) // enviamos el texto en el body
         });
-
+        
         if(!response.ok) throw new Error('Hubo un problema al agregar el comentario');
         const newComment = await response.json();
 
         return newComment;
-    
+        
     } catch (error) {
         console.error('Hubo un error al comentar', error.message);
     }
 }
+
+// Funcion que inicia la vista de detalles y al capturador de clics de votar, comentar.
+const initDetail = async (linkID) => {
+    try {
+        const app = document.getElementById('app'); // obtenemos el contenedor donde insertaremos nuestros componentes
+        app.innerHTML = detailsComponent();  // Inyectamos nuestro componente base
+    
+        // funcion que debe obtener toda la informacion de un link por medio de su ID.
+        const links = await getLink(linkID);
+
+        if (!links) {
+            throw new Error('Error al obtener los detalles');
+        }
+        
+        renderDetails(links); // renderizar la informacion detallada del item seleccionado
+        captureClicksDetails();
+        captureClicksBack();
+    } catch (error) {
+        console.error("Hubo un problema: ", error.message);
+    }
+};
+
+export { initDetail };
